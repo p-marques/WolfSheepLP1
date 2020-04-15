cbe0993d29a568c061f4aacc2f200621179481e8
AronSton265 <AronSton265@users.noreply.github.com>
2020-04-15 23:18:05 +0100

    Board creation
    Added a board and a square class

---
+++ WolfSheepGame/Board.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WolfSheepGameLP1
+{
+	public class Board
+	{
+		public uint Size { get; private set; }
+
+		public BRow[] Rows { get; private set; }
+
+		public Board(uint size)
+		{
+			this.Size = size;
+			CreateRows();
+		}
+
+		public void CreateRows()
+		{
+			this.Rows = new BRow(Size);
+			for (int i = 0; i < Size; i++)
+			{
+				this.Rows[i] = new BRow(i, Size);
+			}
+		}
+
+		public void SetupTable(SheepPlayer sheepPlayer, WoolfPlayer woolfPlayer)
+		{
+			this.Rows[0].Squares[1].PlacePiece(wolfPlayer.WolfPiece);
+
+			for (int i = 0, j = 0; i < Rows.Length; i += 2, j++)
+			{
+				this.Rows[Size - 1].Squares[i].PlacePiece(sheepPlayer.SheepPieces[j]);
+			}
+		}
+
+		public bool IsSquareAvailable(Coord Pos)
+        {
+			bool result = false;
+
+			if((0 <= position.Column && position.Column < Size) && (0 <= position.Row && position.row < Size))
+            {
+
+            }
+        }
+	}
+}
---
+++ WolfSheepGame/Board_Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WolfSheepGameLP1
+{
+	public class BoardSquare
+	{
+		public Coord Pos { get; private set; }
+
+		public Piece Piece { get; get; } = null;
+
+		public bool Playable { get; private set; }
+
+		public BoardSquare(int columnIndex, int rowIndex)
+        {
+			Pos = new Coord(columnIndex, rowIndex);
+
+			IsPlayable = rowIndex % 2 != 0 && columnIndex % 2 == 0 || columnIndex % 2 != 0 && rowIndex % 2 == 0;
+		}
+
+		public PutPiece(Piece piece)
+        {
+			this.Piece = piece;
+
+			piece.BoardSquare = this;
+        }
+	}
+}
