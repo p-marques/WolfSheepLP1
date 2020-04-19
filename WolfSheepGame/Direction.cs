75c8322c67ddbc3be0e4496b535e078530cf2f5b
Pedro Dias Marques <pmark.pt@gmail.com>
2020-04-19 07:56:37 +0100

    Added Direction enum.
    - The Direction enum defines all available input directions.

---
+++ WolfSheepGame/Direction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WolfSheepGameLP1
+{
+    /// <summary>
+    /// Enum that defines all available input directions.
+    /// </summary>
+    public enum Direction
+    {
+        /// <summary>
+        /// No direction.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Go left.
+        /// </summary>
+        Left,
+
+        /// <summary>
+        /// Go right.
+        /// </summary>
+        Right,
+
+        /// <summary>
+        /// Go top and right.
+        /// </summary>
+        TopRight,
+
+        /// <summary>
+        /// Go top and left.
+        /// </summary>
+        TopLeft,
+
+        /// <summary>
+        /// Go bottom and right.
+        /// </summary>
+        BottomRight,
+
+        /// <summary>
+        /// Go bottom and left.
+        /// </summary>
+        BottomLeft
+    }
+}
