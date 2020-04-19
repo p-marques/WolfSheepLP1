10a090be49dc9f94cb87b2222a6811885c5680b6
Pedro Dias Marques <pmark.pt@gmail.com>
2020-04-19 12:45:02 +0100

    Added PlayerInputState enum.
    - Added PlayerInputState that represents the inputs available to the players at any one time.

---
+++ WolfSheepGame/UI/PlayerInputState.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WolfSheepGameLP1.UI
+{
+    /// <summary>
+    /// The state of the player input.
+    /// </summary>
+    public enum PlayerInputState
+    {
+        /// <summary>
+        /// Player can move left and right.
+        /// </summary>
+        LeftAndRight,
+
+        /// <summary>
+        /// Player can move to:
+        /// <list type="bullet">
+        /// <item>Top left.</item>
+        /// <item>Top right.</item>
+        /// <item>Bottom left.</item>
+        /// <item>Bottom right.</item>
+        /// </list>
+        /// </summary>
+        WolfPlayer,
+
+        /// <summary>
+        /// Player can move to:
+        /// <list type="bullet">
+        /// <item>Top left.</item>
+        /// <item>Top right.</item>
+        /// </list>
+        /// </summary>
+        SheepPlayer
+    }
+}
